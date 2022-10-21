f0de55af40d12b22ed552043ed4688cab8d562eb
OblivionCreator <theoblivioncreator@gmail.com>
2022-10-21 02:19:06 +0100

    Ported Bot to Disnake over Discord.Interactions.

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import operator
 import os
 import random
 import time
-import discord.ext.commands
-from discord.ext import commands, tasks
-from discord_components import ComponentsBot, Button, ActionRow
+import disnake.ext.commands
+from disnake.ext import commands, tasks
+from disnake import Button, ActionRow
 
-intents = discord.Intents.default()
+intents = disnake.Intents.default()
 intents.members = True
+intents.message_content = True
 
-bot = ComponentsBot(command_prefix=['h!'], case_insensitive=True, intents=intents)
+bot = commands.Bot(command_prefix=['h!'], case_insensitive=True, intents=intents)
 monsterchannel = 903869168636551218
 
 
@@ -147,12 +148,12 @@ def newEmbed(flavour, monster, time, health, maxhealth, actions='ATTACKS IN...',
     t_deadText = ''
     t_aliveText = ''
     aliveText = "Nobody is fighting!"
-    deadText = "Nobody has fallen!"
+    deadText = "Nobody is wounded!"
     playerDead = False
     playerAlive = False
     if len(hitpoints) < 1:
         aliveText = "Nobody has entered this battle!"
-        deadText = "Nobody has fallen!"
+        deadText = "Nobody is wounded!"
     else:
         for i in hitpoints:
             tempHP = hitpoints[i]
@@ -176,7 +177,7 @@ def newEmbed(flavour, monster, time, health, maxhealth, actions='ATTACKS IN...',
     if playerAlive:
         aliveText = t_aliveText
 
-    embed = discord.Embed(title=f"{flavour}",
+    embed = disnake.Embed(title=f"{flavour}",
                           description=f"{monster}", color=color)
     embed.add_field(name="CURRENT HP", value=f"{health} / {maxhealth}", inline=True)
     if actions: embed.add_field(name=actions, value=f"{time + 1} Seconds", inline=True)
@@ -209,9 +210,9 @@ async def attackHandler(interaction):
         hitpoints[interaction.author.id] = p_hp
         monster_HP = monster_HP - m_dam
     else:
-        sadEmbed = discord.Embed(title='Too Late!',
+        sadEmbed = disnake.Embed(title='Too Late!',
                                  description='The monster has already been killed so you cannot attack it!')
-        interaction.send(embed=sadEmbed)
+        interaction.send(embed=sadEmbed, ephemeral=True)
 
 async def candyHandler(interaction):
     global hitpoints
@@ -231,20 +232,21 @@ async def candyHandler(interaction):
 
     if len(dead_players) == 0 or candyToLose == 0:
         if candyToLose > 0:
-            cEmbed = discord.Embed(title=f'You throw out some candy.', description=f'You threw away {candyToLose} candies. Good job..? You now have {finalcandy} candies.\n_(...what a waste of candy.)_', color=0x388500)
+            cEmbed = disnake.Embed(title=f'You throw out some candy.', description=f'You threw away {candyToLose} candies. Good job..? You now have {finalcandy} candies.\n_(...what a waste of candy.)_', color=0x388500)
         else:
-            cEmbed = discord.Embed(title='You tried to throw out some candy.', description='But you have no candy to throw!', color=0x388500)
+            cEmbed = disnake.Embed(title='You tried to throw out some candy.', description='But you have no candy to throw!', color=0x388500)
     else:
         player = random.choice(dead_players)
         guild = interaction.guild
         user = guild.get_member(player)
-        cEmbed = discord.Embed(title=f'You threw {candyToLose} candies at {user.name}.', description=f'<@{player}> was healed by {candyToLose*2} health!', color=0x65EA00)
-        hitpoints[player] = candyToLose*2
+        cEmbed = disnake.Embed(title=f'You threw {candyToLose} candies at {user.name}.',
+                               description=f'<@{player}> was healed by {candyToLose} health!', color=0x65EA00)
+        hitpoints[player] = candyToLose
         deadlist.remove(player)
 
     addCandies(interaction.author.id, -candyToLose, 0)
 
-    await interaction.send(embed=cEmbed)
+    await interaction.send(embed=cEmbed, ephemeral=True)
 
 async def healHandler(interaction):
     return await heal(interaction, hitpoints[interaction.author.id])
@@ -274,7 +276,7 @@ async def generate(ctx=None, channel=None):
         monster_HP_MAX = int(monster_HP_MAX * 0.75)
     if monster_HP_MAX < 3200:
         monster_HP_MAX = 3200
-    if mname is 'Sans':
+    if mname == 'Sans':
         monster_HP_MAX = 1
     monster_HP = monster_HP_MAX
     turnTime = 60
@@ -289,9 +291,9 @@ async def generate(ctx=None, channel=None):
     lastAttacker = None
 
     message = await channel.send(embed=embed, components=[
-        ActionRow(Button(label="🗡️ Attack Enemy", custom_id=f"attack_enemy", style=4, disabled=False),
-                  Button(label="💊 Heal Yourself", custom_id=f"heal_player", style=3, disabled=False),
-                  Button(label="🍬 Throw Candy", custom_id=f"throw_candy", style=1, disabled=False))])
+        disnake.ui.ActionRow(disnake.ui.Button(label="🗡️ Attack Enemy", custom_id=f"attack_enemy", style=disnake.Color(4), disabled=False),
+                  disnake.ui.Button(label="💊 Heal Yourself", custom_id=f"heal_player", style=disnake.Color(3), disabled=False),
+                  disnake.ui.Button(label="🍬 Throw Candy", custom_id=f"throw_candy", style=disnake.Color(1), disabled=False))])
     while battleOngoing:
         userList = []
         if monster_HP <= 0:
@@ -310,9 +312,9 @@ async def generate(ctx=None, channel=None):
                 await message.edit(
                     embed=newEmbed(flavour, monster, ((battleTime + turnTime) - int(time.time())), monster_HP,
                                    monster_HP_MAX), components=[
-        ActionRow(Button(label="🗡️ Attack Enemy", custom_id=f"attack_enemy", style=4, disabled=False),
-                  Button(label="💊 Heal Yourself", custom_id=f"heal_player", style=3, disabled=False),
-                  Button(label="🍬 Throw Candy", custom_id=f"throw_candy", style=1, disabled=False))])
+        disnake.ui.ActionRow(disnake.ui.Button(label="🗡️ Attack Enemy", custom_id=f"attack_enemy", style=disnake.Color(4), disabled=False),
+                  disnake.ui.Button(label="💊 Heal Yourself", custom_id=f"heal_player", style=disnake.Color(3), disabled=False),
+                  disnake.ui.Button(label="🍬 Throw Candy", custom_id=f"throw_candy", style=disnake.Color(1), disabled=False))])
             except Exception as e:
                 print("Threw Exception! ", e)
             if battleTime + turnTime < int(time.time()):
@@ -323,9 +325,9 @@ async def generate(ctx=None, channel=None):
 
         if monster_HP > 0 and turnCount < 10:
             await message.edit(components=[
-                ActionRow(Button(label="🗡️ Attack Enemy", custom_id=f"attack_enemy", style=4, disabled=True),
-                          Button(label="💊 Heal Yourself", custom_id=f"heal_player", style=3, disabled=True),
-                          Button(label="🍬 Throw Candy", custom_id=f"throw_candy", style=1, disabled=True))])
+                disnake.ui.ActionRow(disnake.ui.Button(label="🗡️ Attack Enemy", custom_id=f"attack_enemy", style=disnake.Color(4), disabled=True),
+                          disnake.ui.Button(label="💊 Heal Yourself", custom_id=f"heal_player", style=disnake.Color(3), disabled=True),
+                          disnake.ui.Button(label="🍬 Throw Candy", custom_id=f"throw_candy", style=disnake.Color(1), disabled=True))])
             attacking = True
             attacked = {}
             attackstring = ''
@@ -363,7 +365,7 @@ async def generate(ctx=None, channel=None):
                         targetValid = True
                         targetList.append(t_attacked)
 
-                if t_crit >= 95:
+                if t_crit >= 98:
                     t_dmg = t_dmg * 2
                 attacked[t_attacked] = t_dmg
                 hitpoints[t_attacked] = hitpoints[t_attacked] - t_dmg
@@ -384,9 +386,9 @@ async def generate(ctx=None, channel=None):
             embed.add_field(name=f'{mname} tried attacking!', value="But they weren't able to attack anyone!",
                             inline=False)
             await message.edit(embed=embed, components=[
-        ActionRow(Button(label="🗡️ Attack Enemy", custom_id=f"attack_enemy", style=4, disabled=True),
-                  Button(label="💊 Heal Yourself", custom_id=f"heal_player", style=3, disabled=True),
-                  Button(label="🍬 Throw Candy", custom_id=f"throw_candy", style=1, disabled=True))])
+        disnake.ui.ActionRow(disnake.ui.Button(label="🗡️ Attack Enemy", custom_id=f"attack_enemy", style=disnake.Color(4), disabled=True),
+                  disnake.ui.Button(label="💊 Heal Yourself", custom_id=f"heal_player", style=disnake.Color(3), disabled=True),
+                  disnake.ui.Button(label="🍬 Throw Candy", custom_id=f"throw_candy", style=disnake.Color(1), disabled=True))])
             await asyncio.sleep(5)
 
         battleTime = int(time.time())
@@ -461,62 +463,62 @@ async def generate(ctx=None, channel=None):
         true_value = round(value * (1 + min(0.5, (rare_value / 200))))
         trueCandyLeaderboard[i] = true_value
 
-        lvEmbed = discord.Embed(title='Level Up!', description='The Following users have levelled up!')
+        lvEmbed = disnake.Embed(title='Level Up!', description='The Following users have levelled up!')
 
         guild = message.guild
         user = guild.get_member(i)
 
         try:
             if 0 < true_value < 100:
-                role = discord.utils.get(guild.roles, id=903193578979868683)
+                role = disnake.utils.get(guild.roles, id=903193578979868683)
                 if role not in user.roles:
                     await user.add_roles(role)
                     if len(lv1Str) < 924:
                         lv1Str = f'{lv1Str}<@{i}>, '
             elif 100 < true_value < 250:
-                role = discord.utils.get(guild.roles, id=903193687171948574)
+                role = disnake.utils.get(guild.roles, id=903193687171948574)
                 if role not in user.roles:
                     await user.add_roles(role)
                     if len(lv2Str) < 924:
                         lv2Str = f'{lv2Str}<@{i}>, '
             elif 250 < true_value < 500:
-                role = discord.utils.get(guild.roles, id=903193808685113385)
+                role = disnake.utils.get(guild.roles, id=903193808685113385)
                 if role not in user.roles:
                     await user.add_roles(role)
                     if len(lv3Str) < 924:
                         lv3Str = f'{lv3Str}<@{i}>, '
             elif 500 < true_value < 1000:
-                role = discord.utils.get(guild.roles, id=903194375360757761)
+                role = disnake.utils.get(guild.roles, id=903194375360757761)
                 if role not in user.roles:
                     await user.add_roles(role)
                     if len(lv4Str) < 924:
                         lv4Str = f'{lv4Str}<@{i}>, '
             elif 1000 < true_value < 1500:
-                role = discord.utils.get(guild.roles, id=903194550175154176)
+                role = disnake.utils.get(guild.roles, id=903194550175154176)
                 if role not in user.roles:
                     await user.add_roles(role)
                     if len(lv5Str) < 924:
                         lv5Str = f'{lv5Str}<@{i}>, '
             elif 1500 < true_value < 2500:
-                role = discord.utils.get(guild.roles, id=903194918208569384)
+                role = disnake.utils.get(guild.roles, id=903194918208569384)
                 if role not in user.roles:
                     await user.add_roles(role)
                     if len(lv6Str) < 924:
                         lv6Str = f'{lv6Str}<@{i}>, '
             elif 2500 < true_value < 5000:
-                role = discord.utils.get(guild.roles, id=904482997556895794)
+                role = disnake.utils.get(guild.roles, id=904482997556895794)
                 if role not in user.roles:
                     await user.add_roles(role)
                     if len(lv7Str) < 924:
                         lv7Str = f'{lv7Str}<@{i}>, '
             elif 5000 < true_value < 10000:
-                role = discord.utils.get(guild.roles, id=905084335747637249)
+                role = disnake.utils.get(guild.roles, id=905084335747637249)
                 if role not in user.roles:
                     await user.add_roles(role)
                     if len(lv8Str) < 924:
                         lv8Str = f'{lv8Str}<@{i}>, '
             elif 10000 < true_value:
-                role = discord.utils.get(guild.roles, id=905088080095109120)
+                role = disnake.utils.get(guild.roles, id=905088080095109120)
                 if role not in user.roles:
                     await user.add_roles(role)
                     if len(lv9Str) < 924:
@@ -607,13 +609,13 @@ async def attack(action, player_hp):
     if crit >= 95:
         r_C += 1
         attackDmg = attackDmg * 3
-        pmEmbed = discord.Embed(title="You attack the enemy!", color=0xFFD800)
+        pmEmbed = disnake.Embed(title="You attack the enemy!", color=0xFFD800)
         pmEmbed.description = f"**CRITICAL HIT!** You deal {attackDmg} damage!\nThe enemy dropped {n_C} candies and {r_C} rare candy. You now have {c_C + n_C} candies and {c_R + r_C} rare candies."
         player_hp = player_hp + HPLoss
         pmEmbed.set_image(
             url='https://media.discordapp.net/attachments/669077343482019870/902003156924379156/tumblr_m0biocwpJC1rnm2iko1_500.png')
     else:
-        pmEmbed = discord.Embed(title="You attack the enemy!", color=0xD95249)
+        pmEmbed = disnake.Embed(title="You attack the enemy!", color=0xD95249)
 
         if player_hp < 0:
             HPLoss = HPLoss + player_hp
@@ -628,7 +630,7 @@ async def attack(action, player_hp):
             pmEmbed.description = f'You deal {attackDmg} damage{dmgText} damage in return.\nYou currently have **{player_hp}/100** health remaining.\nThe enemy dropped {n_C} candies. You now have {c_C + n_C} candies'
         else:
             pmEmbed.description = f'You deal {attackDmg} damage{dmgText} damage in return.\nYou currently have **{player_hp}/100** health remaining.\nThe enemy dropped {n_C} candies and {r_C} rare candy!. You now have {c_C + n_C} candies and {c_R + r_C} rare candies.'
-    await action.send(embed=pmEmbed)
+    await action.send(embed=pmEmbed,ephemeral=True)
 
     addCandies(action.author.id, n_C, r_C)
 
@@ -648,7 +650,7 @@ async def heal(action, hitpoints):
     if hitpoints > 75:
         heal = int(heal * 0.75)
 
-    hlEmbed = discord.Embed(title="You take a moment to heal your wounds.", color=0x59A361)
+    hlEmbed = disnake.Embed(title="You take a moment to heal your wounds.", color=0x59A361)
     hitpoints = min(100, (hitpoints + heal))
     if hitpoints == 69:
         hitpoints = 70
@@ -659,7 +661,7 @@ async def heal(action, hitpoints):
     else:
         hlEmbed.description = f"You were healed by {heal} hitpoints.\nYou now have **{hitpoints}/100** health remaining."
 
-    await action.send(embed=hlEmbed)
+    await action.send(embed=hlEmbed, ephemeral=True)
 
     return hitpoints
 
@@ -669,7 +671,7 @@ async def heal(action, hitpoints):
 async def send(ctx, channel, title, *, message: str):
     currentC, c_r_c = getCandies(ctx.author.id)
     ch = bot.get_channel(int(channel))
-    embed = discord.Embed(title=title, description=message, color=0xff0000)
+    embed = disnake.Embed(title=title, description=message, color=0xff0000)
     await ch.send(embed=embed)
 
 
@@ -681,7 +683,7 @@ async def m_start():
 @bot.command()
 async def start(ctx):
     guild = ctx.guild
-    role = discord.utils.get(guild.roles, id=244328249801310219)
+    role = disnake.utils.get(guild.roles, id=244328249801310219)
 
     if role in ctx.author.roles or ctx.author.id == 110399543039774720:
         await m_start.start()
@@ -690,22 +692,22 @@ async def start(ctx):
 @bot.command()
 async def stop(ctx):
     guild = ctx.guild
-    role = discord.utils.get(guild.roles, id=244328249801310219)
+    role = disnake.utils.get(guild.roles, id=244328249801310219)
 
     if role in ctx.author.roles or ctx.author.id == 110399543039774720:
         m_start.stop()
 
 
-@bot.event
+@bot.listen("on_button_click")
 async def on_button_click(interaction):
     global userList, hitpoints
 
     if turn is False and battleOngoing is True:
-        await interaction.send(content="The monster is currently taking their turn so you cannot act!")
+        await interaction.send(content="The monster is currently taking their turn so you cannot act!", ephemeral=True)
         return
 
     if not battleOngoing:
-        await interaction.send("There's not an ongoing battle!")
+        await interaction.response.send_message("There's not an ongoing battle!", ephemeral=True)
         userList = []
         return
 
@@ -716,22 +718,22 @@ async def on_button_click(interaction):
             hitpoints[interaction.author.id] = 100
 
     if hitpoints[interaction.author.id] <= 0:
-        await interaction.send(content="GAME OVER!\nYou were defeated and have been disengaged from the battle!")
+        await interaction.response.send_message(content="GAME OVER!\nYou were defeated and have been disengaged from the battle!", ephemeral=True)
         return
     if interaction.author.id in userList:
-        await interaction.send(content="You have already acted this turn!")
+        await interaction.response.send_message(content="You have already acted this turn!", ephemeral=True)
         return
     else:
         userList.append(interaction.author.id)
 
-    if interaction.custom_id == 'attack_enemy' and battleOngoing is True:
+    if interaction.component.custom_id == 'attack_enemy' and battleOngoing is True:
         await attackHandler(interaction)
-    elif interaction.custom_id == 'heal_player' and battleOngoing is True:
+    elif interaction.component.custom_id == 'heal_player' and battleOngoing is True:
         hitpoints[interaction.author.id] = await healHandler(interaction)
-    elif interaction.custom_id == 'throw_candy' and battleOngoing is True:
+    elif interaction.component.custom_id == 'throw_candy' and battleOngoing is True:
         await candyHandler(interaction)
     else:
-        await interaction.send("There's not an ongoing battle!")
+        await interaction.response.send_message("There's not an ongoing battle!", ephemeral=True)
         userList = []
 
 
